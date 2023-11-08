5b215ba733a97bab50b3a1028c184af50baeb5d4
Lilian Cardenas <lili.denisse2@gmail.com>
2023-11-08 14:58:53 -0300

    Menu de navegación listo

--- JS/app.js
+++ JS/app.js
@@ -1,8 +1,20 @@
 const btnMenuOpen = document.getElementById("btnMenuOpen");
-const btnMenuClse = document.getElementById("btnMenuClose");
-const menuBar = document.getElementById("menuBar");
+const btnMenuClose = document.getElementById("btnMenuClose");
+const menuBarResponsive = document.getElementById("menuBar");
+const enlaces = document.getElementById("enlaces");
 
 //Abrir menu
 btnMenuOpen.addEventListener("click", function () {
-    menuBar.classList.add("active");
+    menuBarResponsive.classList.add("active");
 });
+
+//Cerrar menu
+btnMenuClose.addEventListener("click", function () {
+    menuBarResponsive.classList.remove("active");
+});
+
+//Cerrar menú con los elementos de enlase
+enlaces.addEventListener("click", function () {
+    menuBarResponsive.style.transitionDelay = "0.5s";
+    menuBarResponsive.classList.remove("active");
+})
